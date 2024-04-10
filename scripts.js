f6331401570ce5e078e04bcb8be40902831c8be4
Akiko0210 <qea643@gmail.com>
2024-04-09 23:55:30 -0700

    generate dots on mouse click

--- scripts.js
+++ scripts.js
@@ -5,12 +5,12 @@
  * also make changes to the HTML and CSS files, but we want you to prioritize
  * demonstrating your understanding of data structures, and you'll do that
  * with the JavaScript code you write in this file.
- * 
+ *
  * The comments in this file are only to help you learn how the starter code
  * works. The instructions for the project are in the README. That said, here
  * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your 
- *    browser and make sure you can see that change. 
+ * - 1 - Change something small in index.html or style.css, then reload your
+ *    browser and make sure you can see that change.
  * - 2 - On your browser, right click anywhere on the page and select
  *    "Inspect" to open the browser developer tools. Then, go to the "console"
  *    tab in the new window that opened up. This console is where you will see
@@ -20,75 +20,120 @@
  * - 3 - Add another string to the titles array a few lines down. Reload your
  *    browser and observe what happens. You should see a fourth "card" appear
  *    with the string you added to the array, but a broken image.
- * 
+ *
  */
 
+class Edge {
+  // edge between A and B;
 
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
+  constructor(A, B) {
+    if (!A.isAvailable || !B.isAvailable) return;
 
-// This is an array of strings (TV show titles)
-let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High"
-];
-// Your final submission should have much more data than this, and 
-// you should use more than just an array of strings to store it all.
+    const div = document.createElement("div");
+    div.classList.add("edge");
+    this.html = div;
+    document.body.appendChild(div);
 
+    const remove = () => {
+      clearInterval(move);
+      this.html.remove();
+    };
 
-// This function adds cards the page to display the data in the array
-function showCards() {
-    const cardContainer = document.getElementById("card-container");
-    cardContainer.innerHTML = "";
-    const templateCard = document.querySelector(".card");
-    
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
+    const update = () => {
+      const rotate = Math.atan((B.y - A.y) / (B.x - A.x));
+      const style = {
+        width: Math.abs(A.x - B.x),
+        height: Math.abs(A.y - B.y),
+        top: A.y,
+        left: A.x,
+        transform: `rotate(calc(${rotate} * 1deg))`,
+      };
+      this.html.style = style;
+    };
 
-        // This part of the code doesn't scale very well! After you add your
-        // own data, you'll need to do something totally different here.
-        let imageURL = "";
-        if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
-        } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
-        } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
-        }
+    update();
 
-        const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, title, imageURL); // Edit title and image
-        cardContainer.appendChild(nextCard); // Add new card to the container
-    }
+    const move = setInterval(() => {
+      if (!A.isAvailable || !B.isAvailable) {
+        remove();
+        return;
+      }
+
+      update();
+    }, 50);
+    // after 2 seconds, this dot will vanish.
+  }
 }
 
-function editCardContent(card, newTitle, newImageURL) {
-    card.style.display = "block";
+class Dot {
+  constructor(x, y, ind) {
+    console.log(x, y);
+    this.x = x;
+    this.y = y;
+    this.ind = ind;
+    this.isAvailable = true;
+    this.edges = [];
+    // set up initial speed
+    this.direction = {
+      dx: (Math.random() - 0.5) * 20,
+      dy: (Math.random() - 0.5) * 20,
+    };
+    this.createHtml();
+    this.addEdges();
+    // every 50ms, this dot will move
+    const move = setInterval(() => {
+      x += this.direction.dx;
+      y += this.direction.dy;
+      this.html.style.left = x + "px";
+      this.html.style.top = y + "px";
+    }, 50);
+    // after 2 seconds, this dot will vanish.
+    setTimeout(() => {
+      clearInterval(move);
+      this.html.remove();
+      this.isAvailable = false;
+      dots = dots.filter((d) => {
+        return d.ind !== this.ind;
+      });
+    }, 2000);
+  }
 
-    const cardHeader = card.querySelector("h2");
-    cardHeader.textContent = newTitle;
+  // create html element
+  createHtml() {
+    const div = document.createElement("div");
+    div.classList.add("dot");
+    div.style.left = this.x + "px";
+    div.style.top = this.y + "px";
+    this.html = div;
+    document.body.appendChild(div);
+  }
 
-    const cardImage = card.querySelector("img");
-    cardImage.src = newImageURL;
-    cardImage.alt = newTitle + " Poster";
+  // distance to another dot 'A'
+  distance(A) {
+    const xdis = this.x - A.x,
+      ydis = this.y - A.y;
+    const dist = Math.sqrt(xdis * xdis + ydis * ydis);
+    return dist;
+  }
 
-    // You can use console.log to help you debug!
-    // View the output by right clicking on your website,
-    // select "Inspect", then click on the "Console" tab
-    console.log("new card:", newTitle, "- html: ", card);
+  addEdges() {
+    const dotsCopy = [...dots];
+    const closestDots = dotsCopy
+      .filter((dot) => dot.index !== this.ind)
+      .sort((A, B) => this.distance(A) - this.distance(B))
+      .slice(0, 4);
+
+    closestDots.forEach((dot) => {
+      this.edges.push(new Edge(this, dot));
+    });
+  }
 }
 
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
-
-function quoteAlert() {
-    console.log("Button Clicked!")
-    alert("I guess I can kiss heaven goodbye, because it got to be a sin to look this good!");
-}
-
-function removeLastCard() {
-    titles.pop(); // Remove last item in titles array
-    showCards(); // Call showCards again to refresh
-}
+let dots = [];
+const createDots = (event) => {
+  //   console.log(event.target);
+  let x = event.clientX;
+  let y = event.clientY;
+  dots.push(new Dot(x, y, dots.length));
+  console.log(dots.length);
+};
